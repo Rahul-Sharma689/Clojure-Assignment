ec173c5ac83295074a987ec28b436b20e5015c62
Rahul Sharma <rsharma4944@altimetrik.com>
2019-03-25 20:51:23 +0530

    chapter-8 assignment

--- chapter-8/src/chapter_8/core.clj
+++ chapter-8/src/chapter_8/core.clj
@@ -2,6 +2,29 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Welcome to Chapter 8 Assignment"))
+ 
+ ;Exercise-1:  Write the macro when-valid so that it behaves similarly to when.
+(defmacro when-valid
+	"Check Validation through when"
+	[to-validate validations-details]
+		`(let [~errors-value (validate ~to-validate ~validations-details)]
+		(if (empty? ~errors-value)
+		(println "It's a success!"))))
+		
+		
+; Exercise-2 : Implement or as a macro.
+(defmacro or
+	"Or implementation as Macro"
+	([] true)
+		([x] x)
+		([x & next]
+		`(let [or# ~x]
+		(if or# or# (or ~@next)))))
+
+; Exercise-3 : Defining Nested attribute.
+(defmacro nested-attr
+   "defining nested attributes"
+   ([attr-name attr]
+    `(def ~attr-name (comp ~attr :attributes)))
