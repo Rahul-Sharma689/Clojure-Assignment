6fc8058b0381da37641f31f78fffad710b3c0a8d
rahul sharma <rahul.sharma@fundingcircle.com>
2019-04-23 19:35:01 +0530

    koans exercises

---
+++ koans/clojur-koans/src/koans/05_sets.clj
@@ -0,0 +1,30 @@
+(ns koans.05-sets
+  (:require [koan-engine.core :refer :all]
+            [clojure.set :as set]))
+
+(meditations
+  "You can create a set by converting another collection"
+  (= #{3} (set __))
+
+  "Counting them is like counting other collections"
+  (= __ (count #{1 2 3}))
+
+  "Remember that a set is a *mathematical* set"
+  (= __ (set '(1 1 2 2 3 3 4 4 5 5)))
+
+  "You can ask clojure for the union of two sets"
+  (= __ (set/union #{1 2 3 4} #{2 3 5}))
+
+  "And also the intersection"
+  (= __ (set/intersection #{1 2 3 4} #{2 3 5}))
+
+  "But don't forget about the difference"
+  (= __ (set/difference #{1 2 3 4 5} #{2 3 5})))
+
+  (= #{3} (set '(3)))
+(= 3 (count #{1 2 3}))
+
+(= (set '(1 2 3 4 5)) (set '(1 1 2 2 3 3 4 4 5 5)))
+  (= #{1 4 3 2 5} (clojure.set/union #{1 2 3 4} #{2 3 5}))
+  (=  #{3 2} (clojure.set/intersection #{1 2 3 4} #{2 3 5}))
+  (= #{4 1} (clojure.set/difference #{1 2 3 4 5} #{2 3 5}))
